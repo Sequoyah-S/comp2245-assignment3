00ceeeb1135df92fa61e5b04d045cb6c9e176acb
Sequoyah <quoyahhs@hotmail.com>
2023-10-17 21:18:42 -0400

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -68,12 +68,36 @@ document.addEventListener("DOMContentLoaded", function () {
       });
       currentPlayer = 'X';
       status.textContent = `Player X's turn`;
-      status.classList.remove('you-won');
+      //status.classList.remove('you-won');
     });
   });
+// Function to check for a win and update the status
+function checkForWin() {
+    const winPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
   
-  
-  
-  
-  
-  
+    for (const pattern of winPatterns) {
+      const [a, b, c] = pattern;
+      if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[b] === gameBoard[c]) {
+        gameWon = true;
+        status.classList.add('you-won');
+        status.textContent = `Congratulations! ${gameBoard[a]} is the Winner!`;
+        return true;
+      }
+    }
+    if (!gameBoard.includes('')) {
+        gameWon = true;
+        //status.textContent = "It's a draw!";
+       // return true;
+      }
+    
+      return false;
+    }
